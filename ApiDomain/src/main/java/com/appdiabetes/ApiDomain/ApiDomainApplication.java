b39323ab0f951c6ed70518ff2f4d40367e2a7c57
Jaime Amilcar Catari Sinsaya <jac.s9325@gmail.com>
2024-03-03 22:09:35 -0500

    Subiendo la Api inicial 03/03/2024 Amilcar

---
+++ ApiDomain/src/main/java/com/appdiabetes/ApiDomain/ApiDomainApplication.java
@@ -0,0 +1,13 @@
+package com.appdiabetes.ApiDomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDomainApplication.class, args);
+	}
+
+}
---
+++ ApiDomain/src/test/java/com/appdiabetes/ApiDomain/ApiDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdiabetes.ApiDomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
